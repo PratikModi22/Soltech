cf8da73d68cce33d114716472ff9578e0edda065
Pratik Modi <142490832+PratikModi22@users.noreply.github.com>
2025-01-23 11:36:53 +0530

    Create rubberproduct.php

---
+++ rubberproduct.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SoltechAboutUs</title>
+    <link rel="shortcut icon" href="https://5.imimg.com/data5/SELLER/Logo/2024/8/442849666/US/UF/EL/226181388/s-120x120.png" type="image/x-icon">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    
+</head>
+</head>
+<body class="p-0 m-0 border-0">
+
+
+
+<?php include'navbar.php'; ?>
+
+
+<section class="container mx-auto p-10 md:py-12 px-0 md:p-8 md:px-0">
+    <section
+        class="p-5 md:p-0 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 xl:grid-cols-3 2xl:grid-cols-4 gap-10 items-start ">
+        <section class="p-5 py-10 bg-purple-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
+            <img src="https://www.dropbox.com/s/mlor33hzk73rh0c/x14423.png?dl=1" alt="">
+            <h1 class="text-3xl my-5">Soft Plushy Cushion Chair</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, incidunt!</p>
+            <h2 class="font-semibold mb-5">$29.99</h2>
+            <button class="p-2 px-6 bg-purple-500 text-white rounded-md hover:bg-purple-600">Add To Cart</button>
+        </section>
+
+        <section class="p-5 py-10 bg-green-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
+            <img src="https://www.dropbox.com/s/8ymeus1n9k9bhpd/y16625.png?dl=1" alt="">
+            <h1 class="text-3xl my-5">Comfortable Wooden Chair</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, incidunt!</p>
+            <h2 class="font-semibold mb-5">$39.99</h2>
+            <button class="p-2 px-6 bg-green-500 text-white rounded-md hover:bg-green-600">Add To Cart</button>
+        </section>
+
+        <section class="p-5 py-10 bg-red-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
+            <img src="https://www.dropbox.com/s/ykdro56f2qltxys/hh2774663-87776.png?dl=1" alt="">
+            <h1 class="text-3xl my-5">Multipurpose Wooden Trolly</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, incidunt!</p>
+            <h2 class="font-semibold mb-5">$19.99</h2>
+            <button class="p-2 px-6 bg-red-500 text-white rounded-md hover:bg-red-600">Add To Cart</button>
+        </section>
+
+        <section class="p-5 py-10 bg-blue-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
+            <img src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1" alt="">
+            <h1 class="text-3xl my-5">Multipurpose Wooden Tool</h1>
+            <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, incidunt!</p>
+            <h2 class="font-semibold mb-5">$34.99</h2>
+            <button class="p-2 px-6 bg-blue-500 text-white rounded-md hover:bg-blue-600">Add To Cart</button>
+        </section>
+    </section>
+</section>
+
+
+
+
+
+
+
+
+
+
+<!-- component -->
+
+
+
+
+
+
+
+
+
+
+<?php include'footer.php'; ?>
+    
+</body>
+</html>
