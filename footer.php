13b119d8a4decdfbd4103647723acbe3cf7249e1
Pratik Modi <142490832+PratikModi22@users.noreply.github.com>
2025-01-23 11:37:40 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,84 +9,26 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 
 </head>
-</head>
-<body>
+<body> 
 
 
 
-<footer class="bg-gray-300 dark:bg-gray-900">
+<footer class="bg-gray-300 h-full dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl">
       <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
         <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://5.imimg.com/data5/SELLER/Logo/2024/8/442849666/US/UF/EL/226181388/s-120x120.png" class="h-16" alt="Flowbite Logo">
+        </a>
             <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class=" hover:underline">About</a>
+            <li class="mb-4">
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SOLTECH RUBBER</span>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Careers</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Brand Center</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Blog</a>
-                </li>
+                    <a href="#">Soltech, led by Ms. Aparna Appa Shitole, is a trusted manufacturing firm established in 2023. With a skilled team and a focus on quality, we deliver reliable products, achieving an annual turnover of ₹40 lakhs to ₹1.5 crores.</a>
+
             </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Discord Server</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Twitter</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Facebook</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Contact Us</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Privacy Policy</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Licensing</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
-            <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">iOS</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Android</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Windows</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">MacOS</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="px-4 py-6 bg-gray-300 dark:bg-gray-700 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">INFINITY™</a>. All Rights Reserved.
-        </span>
-        <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+            <div class="flex mt-4 sm:justify-start md:mt-0 space-x-5 rtl:space-x-reverse">
             <a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                         <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
@@ -118,8 +60,68 @@
                   <span class="sr-only">Dribbble account</span>
               </a>
         </div>
+        </div>
+        <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact Us</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">K-55,Ground Floor,Waluj Midc,Waluj Midc, Aurangabad-431136, Maharashtra, India</a>
+                </li>
+            </ul>
+            
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Phone</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">0987456123</a>
+                </li>
+            </ul>
+
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Email ID</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Demo@Soltech.com</a>
+                </li>
+            </ul>
+             
+        </div>
+        <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Quick Links</h2>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Home</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">About</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Products</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Polymer Range</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Clients</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="overflow-hidden">
+        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Visit us</h2>
+        <iframe class="h-[100px]  items-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.641007776646!2d75.21083949999999!3d19.855137099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb9bc16c37f875%3A0xe1df9d71c6a7b1e9!2sSoltech!5e0!3m2!1sen!2sin!4v1737611737842!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+        </div>
+    </div>
+    <div class="px-4 py-6 bg-gray-300 dark:bg-gray-700 md:flex md:items-center md:justify-between">
+        <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2023 <a href="https://flowbite.com/">Munihelp™</a>. All Rights Reserved.
+        </span>
+       
       </div>
     </div>
+    
   </footer>
 
 
